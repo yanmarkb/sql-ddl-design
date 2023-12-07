6b70946789506acf791a70b3cfc5b43a63fb8926
yberube <yanmarkb@gmail.com>
2023-12-07 13:50:54 -0500

    Added normalization

--- medical_center.sql
+++ medical_center.sql
@@ -12,37 +12,35 @@ CREATE DATABASE fake_medical_center_db;
 
 -- The Doctors table stores information about the doctors, including their ID and name.
 CREATE TABLE Doctors (
-    doctor_id INT PRIMARY KEY UNIQUE, -- Unique identifier for each doctor. DOES PRIMARY KEY IMPLY UNIQUE?
-    doctor_name VARCHAR(100) NOT NULL UNIQUE -- Name of the doctor. Maybe not a good idea to have purely unique names, but this is just an example. Is there an order NOT NULL and UNIQUE have to be in? Discuss with Ben.
+    doctor_id INT PRIMARY KEY, -- Unique identifier for each doctor
+    doctor_name VARCHAR(100) NOT NULL -- Name of the doctor
 );
 
 -- The Patients table stores information about the patients, including their ID and name.
 CREATE TABLE Patients (
-    patient_id INT PRIMARY KEY UNIQUE, -- Unique identifier for each patient
-    patient_name VARCHAR(100) NOT NULL UNIQUE -- Name of the patient
+    patient_id INT PRIMARY KEY, -- Unique identifier for each patient
+    patient_name VARCHAR(100) NOT NULL -- Name of the patient
 );
 
 -- The Visits table stores information about the visits made by patients to doctors, including the visit ID, doctor ID, patient ID, and visit date.
 CREATE TABLE Visits (
-    visit_id INT PRIMARY KEY UNIQUE, -- Unique identifier for each visit
-    doctor_id INT UNIQUE, -- ID of the doctor associated with the visit.  Maybe make this NOT NULL?
-    patient_id INT UNIQUE, -- ID of the patient associated with the visit. This too!
+    visit_id INT PRIMARY KEY, -- Unique identifier for each visit
+    doctor_id INT NOT NULL, -- ID of the doctor associated with the visit
+    patient_id INT NOT NULL, -- ID of the patient associated with the visit
     visit_date DATE, -- Date of the visit.
     FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id) ON DELETE SET NULL, -- Ensures that the doctor ID references a valid doctor in the Doctors table
     FOREIGN KEY (patient_id) REFERENCES Patients(patient_id) ON DELETE SET NULL -- Ensures that the patient ID references a valid patient in the Patients table
 );
 
--- The Diseases table stores information about the diseases diagnosed during the visits, including the disease ID, disease name, and visit ID.
+-- The Diseases table stores information about the diseases diagnosed during the visits, including the disease ID, disease name.
 CREATE TABLE Diseases (
-    disease_id INT PRIMARY KEY UNIQUE, -- Unique identifier for each disease
-    disease_name VARCHAR(100) NOT NULL UNIQUE, -- Name of the disease. Maybe could use PRIMARY KEY here instead of UNIQUE and NOT NULL?
-    visit_id INT, -- ID of the visit associated with the disease
-    FOREIGN KEY (visit_id) REFERENCES Visits(visit_id) ON DELETE SET NULL -- Ensures that the visit ID references a valid visit in the Visits table
+    disease_id INT PRIMARY KEY, -- Unique identifier for each disease
+    disease_name VARCHAR(100) NOT NULL -- Name of the disease
 );
 
 -- The Diagnoses table stores information about the diagnoses made during the visits, including the diagnosis ID, visit ID, disease ID, and notes.
 CREATE TABLE Diagnoses (
-    diagnosis_id INT PRIMARY KEY UNIQUE, -- Unique identifier for each diagnosis
+    diagnosis_id INT PRIMARY KEY, -- Unique identifier for each diagnosis
     visit_id INT, -- ID of the visit associated with the diagnosis
     disease_id INT, -- ID of the disease associated with the diagnosis
     notes VARCHAR(255), -- Additional notes for the diagnosis
@@ -51,6 +49,15 @@ CREATE TABLE Diagnoses (
 );
 
 -- The tables are linked through foreign key constraints to ensure data integrity.
+
+-- Create a table to store the relationship between visits and diseases
+CREATE TABLE VisitDiseases (
+    visit_id INT,
+    disease_id INT,
+    PRIMARY KEY (visit_id, disease_id),
+    FOREIGN KEY (visit_id) REFERENCES Visits(visit_id) ON DELETE CASCADE,
+    FOREIGN KEY (disease_id) REFERENCES Diseases(disease_id) ON DELETE CASCADE
+);
 
 -- Insert sample data into the Doctors table
 INSERT INTO Doctors 
@@ -78,11 +85,11 @@ VALUES
 
 -- Insert sample data into the Diseases table
 INSERT INTO Diseases 
-(disease_id, disease_name, visit_id) 
+(disease_id, disease_name) 
 VALUES
-(1, 'Flu', 1),
-(2, 'Cold', 2),
-(3, 'COVID-19', 3);
+(1, 'Flu'),
+(2, 'Cold'),
+(3, 'COVID-19');
 
 -- Insert sample data into the Diagnoses table
 INSERT INTO Diagnoses 
@@ -91,3 +98,11 @@ VALUES
 (1, 1, 1, 'Patient has high fever'),
 (2, 2, 2, 'Patient has a runny nose'),
 (3, 3, 3, 'Patient tested positive for COVID-19');
+
+-- Insert sample data into the VisitDiseases table
+INSERT INTO VisitDiseases 
+(visit_id, disease_id) 
+VALUES
+(1, 1),
+(2, 2),
+(3, 3);
